cb94e13bcdb0397b27fb349a8f138212d8ba9536
RafaelGoulartB <rafagoulartb@gmail.com>
2023-04-29 16:05:44 -0300

    📦 feat: creating functions

--- hello-world/hello.c
+++ hello-world/hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+int sumNumber (int a, int b) {
+    return a + b;
+}
+
 void main() {
-    printf("Hello World! \n");
+    printf("Hello World! 2 + 2 is equal %i \n", sumNumber(2, 2));
 }
