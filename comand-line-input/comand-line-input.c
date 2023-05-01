acb1ba85d6ea046edf2a9556253d57228caca0e7
RafaelGoulartB <rafagoulartb@gmail.com>
2023-05-01 16:13:01 -0300

    📦 feat: working with command line input

--- comand-line-input/comand-line-input.c
+++ comand-line-input/comand-line-input.c
@@ -2,8 +2,8 @@
 
 void main(int argc, char **argv) {
     printf("Arguments count - %d \n", argc);
-    
-    for(int i=0; i < argc; i++) {
+
+    for (int i = 0; i < argc; i++) {
         printf("arg: %d - %s \n", i, argv[i]);
     }
 }
