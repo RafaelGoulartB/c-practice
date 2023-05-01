a350edf348e9c28d85c50c2a25806d1a8be61eb2
RafaelGoulartB <rafagoulartb@gmail.com>
2023-05-01 16:13:11 -0300

    📦 feat: working with array and pointers

--- pointers-arrays/pointers-arrays.c
+++ pointers-arrays/pointers-arrays.c
@@ -10,25 +10,25 @@ void main() {
     printf("pointer %p \n", pointer);
 
     printf("v[0] = %i the pointer is %p \n", v[0], pointer);
-    printf("v[1] = %i the pointer is %p \n", v[1], pointer+1);
-    printf("v[2] = %i the pointer is %p \n", v[2], pointer+2);
-    printf("v[3] = %i the pointer is %p \n", v[3], pointer+3);
-    printf("v[4] = %i the pointer is %p \n", v[4], pointer+4);
+    printf("v[1] = %i the pointer is %p \n", v[1], pointer + 1);
+    printf("v[2] = %i the pointer is %p \n", v[2], pointer + 2);
+    printf("v[3] = %i the pointer is %p \n", v[3], pointer + 3);
+    printf("v[4] = %i the pointer is %p \n", v[4], pointer + 4);
 
     printf("\n");
     printf("----------- FROM POINTER ----------- \n");
 
     printf("v[0] = %i the pointer is %p \n", *pointer, pointer);
-    printf("v[1] = %i the pointer is %p \n", *(pointer+1), pointer+1);
-    printf("v[2] = %i the pointer is %p \n", *(pointer+2), pointer+2);
-    printf("v[3] = %i the pointer is %p \n", *(pointer+3), pointer+3);
-    printf("v[4] = %i the pointer is %p \n", *(pointer+4), pointer+4);
+    printf("v[1] = %i the pointer is %p \n", *(pointer + 1), pointer + 1);
+    printf("v[2] = %i the pointer is %p \n", *(pointer + 2), pointer + 2);
+    printf("v[3] = %i the pointer is %p \n", *(pointer + 3), pointer + 3);
+    printf("v[4] = %i the pointer is %p \n", *(pointer + 4), pointer + 4);
 
     printf("\n");
     printf("----------- FROM ARRAY POINTER ----------- \n");
     printf("v[0] = %i \n", *(v));
-    printf("v[1] = %i \n", *(v+1));
-    printf("v[2] = %i \n", *(v+2));
-    printf("v[3] = %i \n", *(v+3));
-    printf("v[4] = %i \n", *(v+4));
+    printf("v[1] = %i \n", *(v + 1));
+    printf("v[2] = %i \n", *(v + 2));
+    printf("v[3] = %i \n", *(v + 3));
+    printf("v[4] = %i \n", *(v + 4));
 }
